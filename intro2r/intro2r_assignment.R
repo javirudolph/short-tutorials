a2ae4500beea6ad4a0c2fa6583ae9119f81c435f
javirudolph <javiera.av@ufl.edu>
2019-09-19 05:28:00 -0400

    changes

---
+++ intro2r/intro2r_assignment.R
@@ -0,0 +1,17 @@
+# Assignment
+
+
+
+
+
+
+# 1. Set your working directory to the right place
+
+# 2. Load the csv file with the data
+
+# 3. Calculate some summary statistics
+
+# 4. Visualize the data
+
+# 5. Change parameters in the figures
+
